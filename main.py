84267b0b68fd3dfe477e2ab50a65a84f546ced28
Nimaga95 <117949986+Nimaga95@users.noreply.github.com>
2023-04-13 04:12:14 -0400

    1
    Étape 1 : complete

---
+++ main.py
@@ -0,0 +1,69 @@
+import flask
+from flask import Flask, request, render_template,flash
+import mysql.connector
+
+app = flask.Flask(__name__)
+
+# Configuration de la connexion à la base de données MySQL
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ni4157120162022",
+    database="glo_2005_projet_concessionnairenouvelleauto"
+)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if flask.request.method == 'POST':
+        # Récupérer la requête de l'utilisateur
+        query = flask.request.form['query']
+        print(query)
+
+        # Requête SQL pour sélectionner les données dans la table "articles"
+        cursor = mydb.cursor()
+        sql = "SELECT nomPiece, datePiece FROM glo_2005_projet_concessionnairenouvelleauto.pieces " \
+              "WHERE pieces.categorie LIKE %s OR pieces.nomPiece LIKE %s"
+        cursor.execute(sql, ('%' + query + '%', '%' + query + '%'))
+        results = cursor.fetchall()
+
+        return flask.render_template('exemple.html', results=results, query=query)
+
+    return render_template('barre_recherche.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_upt():
+    if request.method == 'POST':
+        email = request.form['email']
+        firstName = request.form['firstName']
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+
+        if len(email) < 4:
+            flash('Email not valid.', category='error')
+        if len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 8 characters.', category='error')
+        else:
+            flash('Account created.', category='success')
+
+    return render_template("sign-up.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
